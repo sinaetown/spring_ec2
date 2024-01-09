26e668c144353df33c6f2d0e3caa46418300f2b5
sinaetown <ksw08215@gmail.com>
2024-01-09 11:41:07 +0900

    after fixing static folder

--- src/main/java/com/example/demo/post/PostController.java
+++ src/main/java/com/example/demo/post/PostController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 import java.util.*;
 
 @Controller
